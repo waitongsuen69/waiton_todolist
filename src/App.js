90f49c58c85cee08e6a3177e48c6170823d2634e
stargazer <waitongsuen@outlook.com>
2024-04-16 22:50:50 +0930

    new implement of add new task

--- src/App.js
+++ src/App.js
@@ -29,9 +29,17 @@ function App() {
     setTasks(tasksCopy);
   }
 
+  function addTask(context){
+    let new_task = {id : tasks.length +1, text : context, completed : false }
+    // Create a copy of the tasks array
+    let tasksCopy = [...tasks, new_task];
+    setTasks(tasksCopy);  
+    setInputText('');
+  }
+
   // show task list
   // function TaskList({ tasks, toggleTask, deleteTask }) {
-  function TaskList({ tasks, toggleTask }) {
+  function TaskList({ tasks, toggleTask, deleteTask }) {
     // function toggleTask
     return (
       <div>
@@ -57,11 +65,17 @@ function App() {
     { id: 2, text: 'Build a React app', completed: false }
   ]);
 
+  const [inputText, setInputText] = useState('');
+
   return (
     <div>
       <h1>Task Manager</h1>
       {/* <TaskList tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask} /> */}
-      <TaskList tasks={tasks} toggleTask={toggleTask} />
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type='text' name='title' value={inputText} onChange={ e=> setInputText(e.target.value)}></input>
+        <input type='submit' onClick={()=>addTask(inputText)}></input>
+      </form>
+      <TaskList tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask} />
     </div>
   );
 }
